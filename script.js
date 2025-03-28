9c944499fd7d6c9c7a526197d69b00ade1199d95
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-28 14:20:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+
+let btn = document.querySelector("#btn")
+
+
+btn.addEventListener("click", (e)=>{
+	e.preventDefault()
+	let age = document.querySelector("#age").value;
+	let name = document.querySelector("#name").value;
+
+	if(age.length == 0 || name.length == 0){
+		alert("Please enter valid details.")
+		return;
+	}
+
+	const promise = new Promise((res, rej) => {
+		if(age > 18){
+			setTimeout(() => {
+				res("Welcome, . You can vote.")
+			}, 4000)
+		}else{
+			setTimeout(() => {
+				rej("Oh sorry . You aren't old enough.")
+			})
+		}
+	})
+
+	promise.then((data) => alert(data))
+		.catch((err) => alert(err))
+})
